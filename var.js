fc31d2712aae73dfcb7749cbbe9cf865ff41f238
umang agravat <umang@isummation.com>
2024-01-30 15:04:15 +0530

    17 method complate

--- var.js
+++ var.js
@@ -18,8 +18,9 @@ const student = {
 console.log(student.email);
 
 //String methods
-const str ="Umang agravat        ";
+const str ="Umang agravat   ";
 const str1 = 123;
+const str2 ="     hello     "
 
 console.log(str.toLowerCase());
 console.log(str.toUpperCase());
@@ -28,11 +29,16 @@ console.log(str.length);
 console.log(str.slice(0,5));
 console.log(str.concat(str1));
 console.log(str.replace("a", "A"));
-console.log(str.trim());
+console.log(str2.trim());
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
 console.log(str.repeat(4));
 console.log(str[4]);
 console.log(str.charCodeAt(4));
 console.log(str.substr(3,5));
+console.log(str.padStart(40,5));
+console.log(str.padEnd(40,5));
+console.log(str.replaceAll("Umang","Ajay"));
 
 
 
