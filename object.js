dcaa8f88a7271301999c1929dd424e633b2401c0
umang agravat <umang@isummation.com>
2024-01-31 16:59:09 +0530

    Object Methods

--- object.js
+++ object.js
@@ -11,8 +11,7 @@ const person = {
                 recidency : "shivalik"
             }
 };
-
-console.log(person.firstName);
+console.log(person["firstName"]);
 
 //Nested object
 console.log("Using Nested objects");
@@ -54,3 +53,40 @@ const grade = {
 
 const finalResult = Object.assign(course,grade,employee);
 console.log(finalResult);
+
+//Object.create() method
+console.log("Object.create() method");
+const emp ={
+    "id":1,
+    "emp_name": "Tiger Nixon",
+    "emp_salary": 320800,
+    "emp_age": 61,
+    "emp_email": "tiger@gmail.comm"
+};
+console.log(emp);
+
+const obj = Object.create({
+        "id":1,
+        "emp_name": "Tiger Nixon",
+        "emp_salary": 320800,
+        "emp_age": 61,
+        "emp_email": "tiger@gmail.comm"
+});
+console.log(obj);
+delete obj.emp_email; //delete is not peform in .create method
+
+//Object.keys() method
+const key = Object.keys(emp);
+console.log(key);
+
+//Object.values() method
+const value = Object.values(emp);
+console.log(value);
+
+
+
+
+
+
+
+
