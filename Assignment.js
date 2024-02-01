590bf988ae5f1514b69cf2ff2a0991f2fc9d43a3
umang agravat <umang@isummation.com>
2024-02-01 13:12:53 +0530

    Object assignment

--- Assignment.js
+++ Assignment.js
@@ -80,9 +80,20 @@ console.log(store);
 //4. Only print out all the product objects in "store"
 console.log(store["products"]);
 
-//5. Only print out the 3rd product object in "store"
-store.Object.
-console.log(store);
+//5. Only print out the 3rd product object i"n "store"
+console.log(store.products[2]);
 
 //6. Change the price of the banana object through the store object to be 1.75..
+store.products[0].price = 1.75;
 
+//7. Print out both "store" and "banana"
+console.log(store);
+console.log(banana);
+
+//8. Change the price of the candy object directly to be 4.99
+candy.price = 4.99;
+console.log(candy);
+
+//9. Print out both "store" and "candy"
+console.log(store);
+console.log(candy);
