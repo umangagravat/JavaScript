a16e2fec5aa618f885c337de170556457692a8fe
umang agravat <umang@isummation.com>
2024-02-01 11:21:39 +0530

    Assignments

---
+++ Assignment.js
@@ -0,0 +1,18 @@
+//1. Create an object called "mexico" with the following properties
+const mexico = {
+    "id": 24,
+    "name": "Mexico",
+    "capital": "Mexico city",
+    "neighbours": ["USA", "Guatemala", "Belize"]
+};
+
+//2. Print out "mexico" to the terminal
+console.log(mexico);
+
+//3. Change "id" to be 25
+mexico.id = 25;
+console.log(mexico);
+
+//Add "Honduras" to the list of neighbours to "mexico"
+mexico.neighbours.Honduras;
+console.log(mexico);
