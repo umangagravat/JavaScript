17dad7acf51ddf747d3ae314922e4a00ff2714a0
umang agravat <umang@isummation.com>
2024-02-02 12:49:59 +0530

    using for loop

---
+++ Assignment/assignment-6.js
@@ -0,0 +1,8 @@
+const user ={
+    "id": 1,
+    "name": "umang",
+    "company": "isummation"
+};
+
+const obj = Object.entries(user);
+console.log(obj);
---
+++ Assignment/assignment-7.js
@@ -0,0 +1,19 @@
+const user ={
+    "id": 1,
+    "name": "umang",
+    "company": "isummation"
+};
+
+
+const result = [];
+const userkey = Object.keys(user);
+const uservalue = Object.values(user);
+
+for(let i=0; i<userkey.length; i++)
+    {
+        const innervalue = [];
+        innervalue.push(userkey[i]);
+        innervalue.push(uservalue[i]);
+        result.push(innervalue);
+    }
+    console.log(result);
