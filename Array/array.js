591c4b4ee322491ebb5515a910eacce753804f12
umang agravat <umang@isummation.com>
2024-02-05 18:23:02 +0530

    array methods

--- Array/array.js
+++ Array/array.js
@@ -68,6 +68,36 @@ console.log(fruits);
 console.log(fruits1);
 
 
+//flat()
+console.log("<===================flat() Method===================>");
+const myArr = [[1,2,[6,7,[8,9]]],[3,4],[5,11]];
+const newArr = myArr.flat(3);
+console.log(newArr);
+
+//splice() method
+console.log("<===================splice() Method===================>");
+const fruit3 = ["Banana", "Orange", "Apple", "Mango","pinaple","papaya"];
+fruit3.splice(2,2,"lamon","kiwi");
+console.log(fruit3);
+
+//tospliced() method
+console.log("<===================tospliced() Method===================>");
+const fruit4 = ["Banana", "Orange", "Apple", "Mango","pinaple","papaya"];
+const frt = fruit3.splice(2,2,"lamon","kiwi");
+console.log(frt);
+console.log(fruit4);
+
+//Slice() method
+console.log("<===================slice() Method===================>");
+const slice = fruit4.slice(2);
+//const slice = fruit4.slice(1,3);
+console.log(fruit4);
+console.log(slice);
+
+
+
+
+
 
 
 
