bbff9df0709b41c4bd74a3580b3b940cb5313738
umang agravat <umang@isummation.com>
2024-02-06 17:25:43 +0530

    array methods

--- Array/array.js
+++ Array/array.js
@@ -1,4 +1,4 @@
-const cars = ["saab","vlovo","bmw","suzuki","tata","audi","ford","mahendra","isuzu"]
+const cars = ["saab", "vlovo", "bmw", "suzuki", "tata", "audi", "ford", "mahendra", "isuzu"]
 console.log(cars);
 
 
@@ -32,7 +32,7 @@ console.log(cars);
 
 // Aarray push()
 console.log("<===================push() Method===================>");
-let push = cars.push("isuzu");
+let push = cars.push("isuzu", "honda", "scoda");
 console.log(push);
 cars.push("mahendra")
 console.log(cars);
@@ -60,30 +60,30 @@ console.log(cars);
 //concat()
 console.log("<===================concat() Method===================>");
 const fruit = ["apple", "banana", "mango"];
-const fruit1 = ["ornage","kiwi"];
+const fruit1 = ["ornage", "kiwi"];
 const fruit2 = ["pinaple"];
 const fruits = fruit.concat(fruit1);
-const fruits1 = fruit.concat(fruit1,fruit2);
+const fruits1 = fruit.concat(fruit1, fruit2);
 console.log(fruits);
 console.log(fruits1);
 
 
 //flat()
 console.log("<===================flat() Method===================>");
-const myArr = [[1,2,[6,7,[8,9]]],[3,4],[5,11]];
+const myArr = [[1, 2, [6, 7, [8, 9]]], [3, 4], [5, 11]];
 const newArr = myArr.flat(3);
 console.log(newArr);
 
 //splice() method
 console.log("<===================splice() Method===================>");
-const fruit3 = ["Banana", "Orange", "Apple", "Mango","pinaple","papaya"];
-fruit3.splice(2,2,"lamon","kiwi");
+const fruit3 = ["Banana", "Orange", "Apple", "Mango", "pinaple", "papaya"];
+fruit3.splice(2, 0, "kiwi");
 console.log(fruit3);
 
 //tospliced() method
 console.log("<===================tospliced() Method===================>");
-const fruit4 = ["Banana", "Orange", "Apple", "Mango","pinaple","papaya"];
-const frt = fruit3.splice(2,2,"lamon","kiwi");
+const fruit4 = ["Banana", "Orange", "Apple", "Mango", "pinaple", "papaya"];
+const frt = fruit3.splice(2, 2, "lamon", "kiwi");
 console.log(frt);
 console.log(fruit4);
 
@@ -93,6 +93,27 @@ const slice = fruit4.slice(2);
 //const slice = fruit4.slice(1,3);
 console.log(fruit4);
 console.log(slice);
+
+
+console.log("=============Even-odd Number in Array==============")
+const no = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const odd = [];
+const even = [];
+for (let i = 0; i <= no.length - 1; i++) {
+    if (no[i] % 2 == 0) {
+
+        even.push(no[i])
+    }
+    else {
+        odd.push(no[i])
+    }
+}
+console.log(no);
+console.log(even);
+console.log(odd);
+
+
+
 
 
 
