d908631cf0b8ca95668177526033682315b7437c
umang agravat <umang@isummation.com>
2024-02-20 11:13:09 +0530

    JavaScript function to clone an array

---
+++ Array/arrayExercise/exercise2.js
@@ -0,0 +1,12 @@
+// 2. Write a JavaScript function to clone an array.
+// Test Data :
+// console.log(array_Clone([1, 2, 4, 0]));
+// console.log(array_Clone([1, 2, [4, 0]]));
+// [1, 2, 4, 0]
+// [1, 2, [4, 0]]
+
+function array_Clone(arr){
+    return arr.slice(1);
+}
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]]));
