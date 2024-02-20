4ab79b1f904984bab82582f6749fab74b34e127a
umang agravat <umang@isummation.com>
2024-02-20 13:26:11 +0530

    Passing the parameter 'n' will return the last 'n' elements of the array

---
+++ Array/arrayExercise/exercise4.js
@@ -0,0 +1,19 @@
+// 4. Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+function last(array, n) {
+    if (n == null)
+        return array[array.length - 1];
+ 
+    return array.slice(-n);
+};
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2],3));
+console.log(last([7, 9, 0, -2],6));
