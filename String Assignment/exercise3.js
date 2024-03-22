7dcf40cc9c64ba24416aa44825744a201a142e57
umang agravat <umang@isummation.com>
2024-03-22 14:02:43 +0530

    split a string and convert it into an array of words

---
+++ String Assignment/exercise3.js
@@ -0,0 +1,10 @@
+//3. Write a JavaScript function to split a string and convert it into an array of words.
+
+// Test Data :
+// console.log(string_to_array("Robin Singh"));
+// ["Robin", "Singh"]
+console.log("3. Write a JavaScript function to split a string and convert it into an array of words.");
+const  splitArr = function(value){
+    return value.split(" ");
+}
+console.log(splitArr("Umang Agravat"));
