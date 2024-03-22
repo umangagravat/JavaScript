66bae61f9d6616a0c8424d64b73af93338a25858
umang agravat <umang@isummation.com>
2024-03-22 12:19:18 +0530

    retrieve the value of a given property from all elements in an array

---
+++ Array/arrayExercise/exercise27.js
@@ -0,0 +1,15 @@
+// 27. Write a JavaScript function to retrieve the value of a given property from all elements in an array.
+// Sample array : [NaN, 0, 15, false, -22, '',undefined, 47, null]
+// Expected result : [15, -22, 47]
+
+array = [NaN, 0, 15, false, -22, '',undefined, 47, null];
+
+const retrieve = array.filter((element,index,array) => {
+     if (typeof(element) === 'number' && !element == 0){
+        return element;
+     } 
+})
+
+console.log(retrieve);
+
+
