fc2c888e6d193d81d126ded3426877a13f2ecf67
umang agravat <umang@isummation.com>
2024-03-22 15:56:40 +0530

    test whether a string starts with a specified string

---
+++ String Assignment/exercise46.js
@@ -0,0 +1,10 @@
+// 46. Write a JavaScript function to test whether a string starts with a specified string.
+// Test Data :
+// console.log(startsWith('js string exercises', 'js'));
+// true
+
+
+function startsWith(input, string) {
+    return input.startsWith(string);
+  }
+  console.log(startsWith('js string exercises', 'js'));
