80e9a48e32467aece961c4e68bac8812d6bbd302
umang agravat <umang@isummation.com>
2024-03-22 15:53:39 +0530

    remove non-printable ASCII characters

---
+++ String Assignment/exercise42.js
@@ -0,0 +1,10 @@
+// 42. Write a JavaScript function to uncapitalize each word in the string.
+// Test Data :
+// console.log(unCapitalizeWords('JS STRING EXERCISES'));
+// "js string exercises"
+
+function unCapitalize(str)
+{
+ return str.toLowerCase();
+}
+console.log(unCapitalize('JS STRING EXERCISES'));
